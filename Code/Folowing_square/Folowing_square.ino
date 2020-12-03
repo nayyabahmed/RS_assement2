7798cc396e207ca3bbf4425d81c3f3801c0b07af
nayyabahmed <nayyabahmed@hotmail.com>
2020-12-03 20:31:07 +0000

    Updated the power Input function to take the negative values

--- Code/Folowing_square/Folowing_square.ino
+++ Code/Folowing_square/Folowing_square.ino
@@ -6,7 +6,7 @@
 #define R_DIR_PIN 15
 
 #define TURN_PWR 40
-#define PWR 40 
+#define PWR 40
 #define SQR_t 3270
 
 bool t = true;
@@ -51,60 +51,43 @@ void setup() {
 
 
 
-void go_forward(int t) {
 
-  if (time_elapsed < 1500)
-  {
-    powerInput(0, 40);
-  }
-
-  /*else if (time_elapsed < (t + 11000))
-  {
-    powerInput(-30, -32);
-
-  }*/
-  else
-  {
-    powerInput(0, 0);
-  }
-
-}
 
 void move_in_square(int t)
-{ 
-  int r=1500;
+{
+  int r = 1500;
 
   if (time_elapsed < t)
   {
     powerInput(PWR, PWR);
   }
-  else if (time_elapsed < (t+r) )
+  else if (time_elapsed < (t + r) )
   {
     powerInput(0, TURN_PWR);
   }
 
-  else if (time_elapsed < (t*2+r))
+  else if (time_elapsed < (t * 2 + r))
   {
     powerInput(PWR, PWR);
   }
 
-  else if (time_elapsed < (t*2+r*2))
+  else if (time_elapsed < (t * 2 + r * 2))
   {
     powerInput(0, TURN_PWR);
   }
-  else if (time_elapsed < (t*3+r*2))
+  else if (time_elapsed < (t * 3 + r * 2))
   {
-    powerInput(PWR, PWR+2);
+    powerInput(PWR, PWR + 2);
   }
-  else if (time_elapsed < (t*3+r*3))
+  else if (time_elapsed < (t * 3 + r * 3))
   {
     powerInput(0, TURN_PWR);
   }
-  else if (time_elapsed < (t*4+r*3))
+  else if (time_elapsed < (t * 4 + r * 3))
   {
     powerInput(PWR, PWR);
   }
-  else if (time_elapsed < (t*4+r*4))
+  else if (time_elapsed < (t * 4 + r * 4))
   {
     powerInput(0, TURN_PWR);
   }
@@ -122,13 +105,12 @@ void loop() {
   time_now = millis();
   time_elapsed = time_now - last_timestamp;
 
-  //powerInput(20,20);
 
   // Adjust power. e.g., increment by 4 on every loop()
   //speedChange();
 
-  //go_forward(SQR_t); //Start and move for a period and then comes back to statrting point
-  move_in_square(SQR_t); // function moves the robot in a square an then stops
+  go_forward(SQR_t); //Start and move for a period and then comes back to statrting point
+  //move_in_square(SQR_t); // function moves the robot in a square an then stops
 
   // Send power PWM to pins, to motor drivers.
   analogWrite( L_PWM_PIN, l_power );
@@ -151,41 +133,63 @@ void print_debug()
   delay(250); // Brief pause
 }
 
-void powerInput(float l, float r)
+void go_forward(int t) 
 {
 
+  if (time_elapsed < 700)
+  {
+    powerInput(-40, 40);
+  }
 
+  /*else if (time_elapsed < (t + 11000))
+    {
+    powerInput(-30, -32);
 
-  if (l >= 0)
+    }*/
+  else
+  {
+    powerInput(0, 0);
+  }
+
+}
+
+void powerInput(float l, float r)
+{
+  bool c1 = ( (l >= 0) && (r >= 0) );
+  bool c2 = ( (l < 0) && (r < 0) );
+  bool c3 = ( (l < 0) && (r >= 0) );
+  bool c4 = ( (l >= 0) && (r < 0) );
+
+  if (c1) 
   {
     l_power = l;
-    digitalWrite( L_DIR_PIN, LOW  );
-
-  }
-  if (r >= 0)
-  {
     r_power = r;
+    digitalWrite( L_DIR_PIN, LOW  );
     digitalWrite( R_DIR_PIN, LOW );
   }
 
-
-  if (l < 0)
+  if (c2)
   {
     l_power = l * -1;
-    digitalWrite( L_DIR_PIN, HIGH  );
-
-  }
-  if (r < 0)
-  {
     r_power = r * -1;
+    digitalWrite( L_DIR_PIN, HIGH  );
     digitalWrite( R_DIR_PIN, HIGH );
   }
 
-  if ((l < 0 && r > 0) || (l > 0 && r < 0))
+  if (c3)
   {
-    l_power = 0;
-    r_power = 0;
+    l_power = l * -1;
+    r_power = r ;
+    digitalWrite( L_DIR_PIN, HIGH  );
+    digitalWrite( R_DIR_PIN, LOW );
+  }
 
+  if (c4)
+  {
+    l_power = l;
+    r_power = r * -1;
+    digitalWrite( L_DIR_PIN, LOW );
+    digitalWrite( R_DIR_PIN, HIGH );
   }
 
 }
